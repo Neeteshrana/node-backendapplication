d7da37f141672b56f64f2a6a73a1d8ad0890328a
Neeteshrana <110087660+Neeteshrana@users.noreply.github.com>
2025-05-07 19:49:16 -1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('App is running!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
